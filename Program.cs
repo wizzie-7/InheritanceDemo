be71d8775eea67b909a9a60efe4f646e37cb7b9f
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:52:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using InhertanceDemoWork;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftEng SE = new SoftEng();
+
+
+            Employee SE1 = new SoftEng();
+            SE1.fname = "asd";
+            SE1.lname = "lkj";
+            //SE1.Designation = "soft engg" // can not call 
+            //SE1.printSoftEnggDetails(); // can not call method
+            Employee SSE1 = new SenSoftEngg();
+            SSE1.fname = "asd";
+            SSE1.lname = "lkj";
+            //SSE.Designation = "soft engg" // can not call 
+            //SSE.printSoftEnggDetails(); // can not call method
+            //SoftEng SE2 = new Employee(); //
+        }
+    }
+}
